eecddceb321b533d5727111e776df0101ba1d54f
Esraa Gamal <esraa.gamal@mdi.com.eg>
2023-01-23 20:51:11 +0200

    intiaal commit

---
+++ 3arabity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3arabity
+//
+//  Created by Esraa Gamal on 15/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
